a4166a79513287cf4e7acf8645aaf1f5e46129d1
hellosirandy <sac840711@gmail.com>
2017-05-28 20:27:46 +0800

    add static files

--- webcam/views.py
+++ webcam/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+
+app_name = 'webcam'
 
 def index(request):
-    template = loader.get_template('')
-    return HttpResponse("<h1>This is homepage</h1>")
+    context = {}
+    return render(request, 'webcam/index.html', context)
