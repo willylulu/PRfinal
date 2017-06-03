ec7c4c4fa5821523720ad7054ad99b73d98f07b2
hellosirandy <sac840711@gmail.com>
2017-06-03 11:57:10 +0800

    finish post

--- webcam/views.py
+++ webcam/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 app_name = 'webcam'
 
 def index(request):
     context = {}
     return render(request, 'webcam/index.html', context)
+
+def video(request):
+    print (request.POST['content'])
+    return redirect('/webcam/')
