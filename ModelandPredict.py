9fdfe9459d51caa3ca578c61a54c531e37b3166a
hellosirandy <sac840711@gmail.com>
2017-06-12 14:28:03 +0800

    clean server

--- ModelandPredict.py
+++ ModelandPredict.py
@@ -3,6 +3,8 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.utils import np_utils
 from keras import initializers
+# from keras import backend as K
+# K.set_image_dim_ordering('th')
 
 import os
 import numpy as np
@@ -17,27 +19,27 @@ class VGGmodel(object):
     def __init__(self):
         """Return a Customer object whose name is *name* and starting
         balance is *balance*."""
-        self.model = Sequential()        
-        data = sio.loadmat('caltech_image.mat')
+        self.model = Sequential()
+        data = sio.loadmat(os.path.join(os.path.dirname(__file__), 'caltech_image.mat'))
         self.categories = data['categories']
         self.num_classed = len(self.categories)
-        
+
     def load_model(self):
         # load json and create model
-        json_file = open('caltech/model.json', 'r')
+        json_file = open(os.path.join(os.path.dirname(__file__), 'caltech/model.json'), 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         self.model = model_from_json(loaded_model_json)
         # load weights into new model
-        self.model.load_weights("caltech/model.h5")
+        self.model.load_weights(os.path.join(os.path.dirname(__file__), "caltech/model.h5"))
         print("Loaded model from disk")
-    
+
     def model_compile(self):
         epochs = 50
         adam = Adam(lr=0.0001)
         self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
         print(self.model.summary())
-    
+
     def model_predict(self, figure):
         if len(figure.shape) == 2:
             figure = np.transpose(np.array([figure, figure, figure]), (2, 0, 1))
@@ -54,9 +56,9 @@ class VGGmodel(object):
             return(self.categories[classed[0]])
         else:
             return ''
-    
+
     def test(self):
-        data = sio.loadmat('caltech_del.mat')
+        data = sio.loadmat(os.path.join(os.path.dirname(__file__), 'caltech_del.mat'))
         X_test = data['X_test']
         y_test = data['y_test']
         scores = self.model.evaluate(X_test, y_test, verbose=0)
@@ -71,7 +73,7 @@ class CIFARmodel(object):
         self.model = Sequential()
         self.num_classed = num_classed
         self.categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-        
+
     def load_model(self):
         # load json and create model
         json_file = open('cifar10/model.json', 'r')
@@ -81,14 +83,14 @@ class CIFARmodel(object):
         # load weights into new model
         self.model.load_weights("cifar10/model.h5")
         print("Loaded model from disk")
-    
+
     def model_compile(self):
         epochs = 50
         lrate = 0.01
         adam = Adam(lr=0.001)
         self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
         print(self.model.summary())
-    
+
     def model_predict(self, figure):
         test = misc.imresize(figure, (32,32,3)).transpose(2,0,1)
         test = test.astype('float32')
@@ -100,7 +102,7 @@ class CIFARmodel(object):
             return(self.categories[classed[0]])
         else:
             return ''
-    
+
     def test(self):
         (X_train, y_train), (X_test, y_test) = cifar10.load_data()
         X_test = X_test.astype('float32')
@@ -116,47 +118,46 @@ class CIFAR100model(object):
     def __init__(self, num_classed= 10):
         """Return a Customer object whose name is *name* and starting
         balance is *balance*."""
-        self.model = Sequential()
         self.num_classed = num_classed
         self.categories = ['apples', 'aquarium fish', 'baby', 'bear', 'beaver',
             'bed', 'bee', 'beetle', 'bicycle', 'bottles',
-            'bowls','boy', 'bridge', 'bus', 'butterfly', 
-            'camel', 'cans', 'castle', 'caterpillar', 'cattle', 
-            'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 
-            'couch', 'crab', 'crocodile', 'cups', 
-            'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 
-            'fox', 'girl', 'hamster', 'house', 'kangaroo', 'computer keyboard', 'lamp', 
-            'lawn-mower', 'leopard', 'lion', 'lizard', 'lobster', 
-            'man', 'maple', 'motorcycle', 'mountain', 'mouse', 
-            'mushrooms', 'oak', 'oranges', 'orchids', 'otter', 
-            'palm', 'pears', 'pickup truck', 'pine', 'plain', 
-            'plates', 'poppies', 'porcupine', 'possum', 'rabbit', 
-            'raccoon', 'ray', 'road', 'rocket', 'roses', 'sea', 
-            'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 
-            'snail', 'snake', 'spider', 'squirrel', 'streetcar', 
-            'sunflowers', 'sweet peppers', 'table', 'tank', 'telephone', 
-            'television', 'tiger', 'tractor', 'train', 'trout', 
-            'tulips', 'turtle', 'wardrobe', 'whale', 'willow', 
+            'bowls','boy', 'bridge', 'bus', 'butterfly',
+            'camel', 'cans', 'castle', 'caterpillar', 'cattle',
+            'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach',
+            'couch', 'crab', 'crocodile', 'cups',
+            'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest',
+            'fox', 'girl', 'hamster', 'house', 'kangaroo', 'computer keyboard', 'lamp',
+            'lawn-mower', 'leopard', 'lion', 'lizard', 'lobster',
+            'man', 'maple', 'motorcycle', 'mountain', 'mouse',
+            'mushrooms', 'oak', 'oranges', 'orchids', 'otter',
+            'palm', 'pears', 'pickup truck', 'pine', 'plain',
+            'plates', 'poppies', 'porcupine', 'possum', 'rabbit',
+            'raccoon', 'ray', 'road', 'rocket', 'roses', 'sea',
+            'seal', 'shark', 'shrew', 'skunk', 'skyscraper',
+            'snail', 'snake', 'spider', 'squirrel', 'streetcar',
+            'sunflowers', 'sweet peppers', 'table', 'tank', 'telephone',
+            'television', 'tiger', 'tractor', 'train', 'trout',
+            'tulips', 'turtle', 'wardrobe', 'whale', 'willow',
             'wolf', 'woman', 'worm']
         print(len(self.categories))
-        
+
     def load_model(self):
         # load json and create model
-        json_file = open('cifar100/100model_70.json', 'r')
+        json_file = open(os.path.join(os.path.dirname(__file__), 'model/cifar100/100model_70.json'), 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         self.model = model_from_json(loaded_model_json)
         # load weights into new model
-        self.model.load_weights("cifar100/100model_70.h5")
+        self.model.load_weights(os.path.join(os.path.dirname(__file__), "model/cifar100/100model_70.h5"))
         print("Loaded model from disk")
-    
+
     def model_compile(self):
         epochs = 50
         lrate = 0.01
         adam = Adam(lr=0.001)
         self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
         print(self.model.summary())
- 
+
     def normalize(self, X_test):
         mean = 121.936
         std = 68.389
@@ -175,7 +176,7 @@ class CIFAR100model(object):
             return(self.categories[classed[0]])
         else:
             return ''
-    
+
     def test(self):
         (X_train, y_train), (X_test, y_test) = cifar100.load_data()
         X_test = X_test.astype('float32')
--- PRfinal/settings.py
+++ PRfinal/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'webcam.apps.WebcamConfig',
-    'music.apps.MusicConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
